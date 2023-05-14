c5d7b57f3605c44584b821cd8315c26d849e62ce
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-14 12:44:55 +0000

    add: util for getting shape of a tensor as list

---
+++ pggan/models/utils.py
@@ -0,0 +1,15 @@
+from typing import List
+
+import tensorflow as tf
+
+
+def get_shape(input_tensor: tf.Tensor) -> List[int]:
+    dynamic_shape = tf.shape(input_tensor)
+    return (
+        dynamic_shape
+        if input_tensor.shape == tf.TensorShape(None)
+        else [
+            dynamic[i] if s is None else s
+            for i, s in enumerate(input_tensor.shape.as_list())
+        ]
+    )
